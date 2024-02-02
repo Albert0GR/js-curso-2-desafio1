bc3ba54ad4f2cbfdcadcc4219d240ad81a39208d
Albert0GR <102188547+Albert0GR@users.noreply.github.com>
2024-02-01 18:43:10 -0600

    desafio 1.1 y 1.2

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 
+//Cambia el contenido de la etiqueta h1 con document.querySelector y asigna el siguiente texto: "Hora del Desafío".
+let titulo = document.querySelector('h1') 
+
+titulo.innerHTML = "Hora del Desafío"
+
+//Crea una función que muestre en la consola el mensaje 
+//"El botón fue clicado" siempre que se presione el botón "Console".
+
+function botonConsole(){
+    console.log('El botón fue clicado');
+}
+
+
+
+
+
